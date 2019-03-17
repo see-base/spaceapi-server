e116d47d7d55c78329c07e594a4b2877282d8f05
root <root@see-base.de>
2019-03-17 19:36:14 +0100

    Add c3woc space

---
+++ c3woc-css.sh
@@ -0,0 +1,42 @@
+echo "$(date) $1" >> /tmp/foo
+
+# Select random line from file using reservoir sampling, first argument should be filename.
+function randomLine {
+    local selectedLine=''
+    local i=1
+    
+    while read -r line; do
+    
+        # If line is empty, ignore.
+        if [ -z "$line" ]; then
+            continue
+        fi
+    
+        # If selected line is empty, use this
+        if [ -z "$selectedLine" ]
+        then
+            selectedLine=$line
+        else
+            # Replace selected Line with probability 1/i
+            if [ "$(($RANDOM % $i))" -eq "0" ]; then
+                selectedLine=$line
+            fi
+        fi
+        
+        ((i++))
+    done < "$1"
+   
+    echo "$selectedLine"
+}
+
+# Space toolbox open/close:
+if [ $1 = "open" ]; then
+    echo "#header h1 a.icon:before { color: lime; }" > /var/www/bodensee.space/web/spaceapi/c3woc/space_api.css
+    echo "#navButton .toggle:before { color: lime; }" >> /var/www/bodensee.space/web/spaceapi/c3woc/space_api.css
+    echo "#tb-opened { display: blocked; } #tb-closed { display: none !important; }" > /var/www/bodensee.space/web/c3woc/toolbox/spacestatus.css
+else
+    echo "#header h1 a.icon:before { color: inherit; }" > /var/www/bodensee.space/web/spaceapi/c3woc/space_api.css
+    echo "#navButton .toggle:before { color: #fff; }" > /var/www/bodensee.space/web/spaceapi/c3woc/space_api.css
+    echo "#tb-closed { display: blocked; } #tb-opened { display: none !important; }" > /var/www/bodensee.space/web/spaceapi/c3woc/spacestatus.css
+fi
+
