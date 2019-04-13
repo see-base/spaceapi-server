2072b04e86bc732e41fd365a90a0f9a6f41f70a1
root <root@see-base.de>
2019-04-14 00:28:14 +0200

    Don't spam to SLACK

--- toolbox-css.sh
+++ toolbox-css.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "$(date) $1" >> /tmp/foo
 
 # Select random line from file using reservoir sampling, first argument should be filename.
@@ -31,18 +32,24 @@ function randomLine {
 
 # Load config values
 source /usr/lib/cgi-bin/spaceapi-server/token.conf
+if [ ! -f /var/www/bodensee.space/web/spaceapi/toolbox/space-toolbox.conf ]; then
+    toolbox_open=false
+else
+    source /var/www/bodensee.space/web/spaceapi/toolbox/space-toolbox.conf
+fi
+
 # Space toolbox open/close:
 if [ $1 = "open" ]; then
+    echo "toolbox_open=true" > /var/www/bodensee.space/web/spaceapi/toolbox/space-toolbox.conf
     echo "#header h1 a.icon:before { color: lime; }" > /var/www/bodensee.space/web/spaceapi/toolbox/space_api.css
     echo "#navButton .toggle:before { color: lime; }" >> /var/www/bodensee.space/web/spaceapi/toolbox/space_api.css
     echo "#tb-opened { display: blocked; } #tb-closed { display: none !important; }" > /var/www/bodensee.space/web/spaceapi/toolbox/spacestatus.css
-    echo "open" > /tmp/space-toolbox.log
-    curl -X POST -H 'Content-type: application/json' --data "{'text':'$(randomLine '/usr/lib/cgi-bin/spaceapi-server/messages_open.txt')'}" https://hooks.slack.com/services/$TOKEN1/$TOKEN2/$TOKEN3 > /dev/null
+    if [ "$toolbox_open" = false ]; then curl -X POST -H 'Content-type: application/json' --data "{'text':'$(randomLine '/usr/lib/cgi-bin/spaceapi-server/messages_open.txt')'}" https://hooks.slack.com/services/$TOKEN1/$TOKEN2/$TOKEN3 > /dev/null; fi
 else
+    echo "toolbox_open=false" > /var/www/bodensee.space/web/spaceapi/toolbox/space-toolbox.conf
     echo "#header h1 a.icon:before { color: inherit; }" > /var/www/bodensee.space/web/spaceapi/toolbox/space_api.css
     echo "#navButton .toggle:before { color: #fff; }" > /var/www/bodensee.space/web/spaceapi/toolbox/space_api.css
     echo "#tb-closed { display: blocked; } #tb-opened { display: none !important; }" > /var/www/bodensee.space/web/spaceapi/toolbox/spacestatus.css
-    echo "closed" > /tmp/space-toolbox.log
-    curl -X POST -H 'Content-type: application/json' --data "{'text':'$(randomLine '/usr/lib/cgi-bin/spaceapi-server/messages_closed.txt')'}" https://hooks.slack.com/services/$TOKEN1/$TOKEN2/$TOKEN3 >/dev/null 
+    if [ "$toolbox_open" = true ]; then curl -X POST -H 'Content-type: application/json' --data "{'text':'$(randomLine '/usr/lib/cgi-bin/spaceapi-server/messages_closed.txt')'}" https://hooks.slack.com/services/$TOKEN1/$TOKEN2/$TOKEN3 >/dev/null;  fi
 fi
 
